ec2cd8f6b107c6ad1ae6cde34bd75a99433c05c7
MalardD <35556894+MalardD@users.noreply.github.com>
2019-11-17 23:14:40 +0100

    Add files via upload

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,16 +1,23 @@
 <!-- Utiliser pour une question-->
 <template>
   <div>
+    <!-- Affichage de l'intitulé de la question -->
     <h1>{{libelle}}</h1>
+
+    <!-- Affichage des reponses sous la forme de case à cocher-->
     <el-checkbox-group v-model="reponse">
       <el-checkbox v-for="option in optionReponse" v-bind:key="option" :label="option" @click="action(index)">{{option}}</el-checkbox>
     </el-checkbox-group>
     {{reponse}}
+
+    <!-- Bouton permettant la navigation entre les pages 
+    ps: Ne fonctionne pas-->
     <el-button-group>
       <el-button type="primary" icon="el-icon-arrow-left">Question précédente</el-button>
       <el-button type="primary">Question suivante<i class="el-icon-arrow-right el-icon-right"></i></el-button>
     </el-button-group>
 
+    <!-- Message affichant le score mais ne fonctionne pas-->
     <b-alert v-if="fin" show>Votre score est : {{ score }} / {{ questions.length }}</b-alert>
   </div>
 </template>

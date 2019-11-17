eabd48150e7b197ce692b761766688fe0db4021e
MalardD <35556894+MalardD@users.noreply.github.com>
2019-11-17 20:43:57 +0100

    Add files via upload

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,32 +1,37 @@
 <!-- Utiliser pour une question-->
-
-
-
 <template>
-<div>
-    <h1>{{intitule}}</h1>
-    <ui-checkbox v-model="checked" >{{reponse[0]}}</ui-checkbox> 
-    <ui-checkbox v-model="checked" >{{reponse[1]}}</ui-checkbox> 
-</div>
+  <div>
+    <h1>{{libelle}}</h1>
+    <el-checkbox-group v-model="reponse">
+      <el-checkbox v-for="option in optionReponse" v-bind:key="option" :label="option" @click="action(index)">{{option}}</el-checkbox>
+    </el-checkbox-group>
+    {{reponse}}
+    <el-button-group>
+      <el-button type="primary" icon="el-icon-arrow-left">Question précédente</el-button>
+      <el-button type="primary">Question suivante<i class="el-icon-arrow-right el-icon-right"></i></el-button>
+    </el-button-group>
+
+    <b-alert v-if="fin" show>Votre score est : {{ score }} / {{ questions.length }}</b-alert>
+  </div>
 </template>
 
 <script>
-
-var app = new Vue()
-export default {
+  export default {
     name: 'question',
     props: {
         libelle: String,
         reponse: String,
+        optionReponse: Array
     },
-    data(){
+     
+    data (){
         return{
-            libelle: this.libelle,
-            reponse: this.reponse
+             libelle: this.libelle,
+             reponse: this.reponse,
+             optionReponse: this.optionReponse
         }
     }
     
   
-    
-}
+  }    
 </script>
